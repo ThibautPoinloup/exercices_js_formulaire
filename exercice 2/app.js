8a2047b705924f7e2856868bf91de956e136ffec
thibaut-poinloup <poinloupthibaut@gmail.com>
2023-01-19 14:57:52 +0100

    Ajout js
    l'erreur fonctionne sur mes <option>

--- exercice 2/app.js
+++ exercice 2/app.js
@@ -4,30 +4,26 @@ form.addEventListener('submit', (event) => {
     
     event.preventDefault();
 
-    const firstNameInput = document.getElementById('firstName');
-    const lastNameInput = document.getElementById('lastName');
-    const adressInput = document.getElementById('adress');
-    const phoneInput = document.getElementById('phone');
-    const productsInput = document.getElementById('products');
-    const sizeInput = document.getElementById('size');
-    const totalPrice = document.getElementById('total');
-    const optionProduct = document.getElementsByClassName('optionProduct')
-    const optionSize = document.getElementsByClassName('optionSize')
-    
+    const firstName = document.getElementById('firstName');
+    const lastName = document.getElementById('lastName');
+    const address = document.getElementById('address');
+    const phone = document.getElementById('phone');
+    const products = document.querySelector('#products');
+    const size = document.querySelector('#size');
 
     const formData = {
-        firstName: firstNameInput.value,
-        lastName: lastNameInput.value,
-        adress: adressInput.value,
-        phone: phoneInput.value,
-        products: productsInput.value,
-        size: sizeInput.value
+        firstName: firstName.value,
+        lastName: lastName.value,
+        address: address.value,
+        phone: phone.value,
+        products: products.value,
+        size: size.value
     }
 
     const errors = {
         firstName: false,
         lastName: false,
-        adress: false,
+        address: false,
         phone: false,
         products: false,
         size: false
@@ -35,64 +31,56 @@ form.addEventListener('submit', (event) => {
 
     const firstNameError = document.getElementById('firstNameError');
     const lastNameError = document.getElementById('lastNameError');
-    const adressError = document.getElementById('adressError');
+    const addressError = document.getElementById('addressError');
     const phoneError = document.getElementById('phoneError');
     const productsError = document.getElementById('productsError');
     const sizeError = document.getElementById('sizeError');
 
     firstNameError.style.display = 'none';
     lastNameError.style.display = 'none';
-    adressError.style.display = 'none';
+    addressError.style.display = 'none';
     phoneError.style.display = 'none';
     productsError.style.display = 'none';
     sizeError.style.display = 'none';
 
-    if(!formData.firstName || !formData.lastName || !formData.adress || !formData.phone || !formData.products || !formData.size){
-        const nameRegex = /^[a-zA-Z ]+$/;
-        const phoneRegex = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/;
-       
-
-        if (!formData.firstName || !nameRegex.test(formData.firstName)) {
+    if(!formData.firstName || !formData.lastName || !formData.address || !formData.phone || !formData.products || !formData.size){
+        const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
+        const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+        const addressRegex = /(\d{1,}) [a-zA-Z0-9\s]+(\.)? [a-zA-Z]+(\,)? [A-Z]{2} [0-9]{5,6}/g;
+        
+        if(!formData.firstName || !nameRegex.test(formData.firstName)){
             errors.firstName = true;
             firstNameError.style.display = 'block';
         }
 
-        if (!formData.lastName || !nameRegex.test(formData.lastName)) {
+        if(!formData.lastName || !nameRegex.test(formData.lastName)){
             errors.lastName = true;
             lastNameError.style.display = 'block';
         }
 
-        if (!formData.phone || !phoneRegex.test(formData.phone)) {
+        if(!formData.address || addressRegex.test(formData.address)){
+            errors.address = true;
+            addressError.style.display = 'block';
+        }
+
+        if(!formData.phone || !phoneRegex.test(formData.phone)){
             errors.phone = true;
             phoneError.style.display = 'block';
         }
-        if (!formData.adress) {
-            errors.adress = true;
-            adressError.style.display = 'block';
-        }
 
-        // if (!formData.products) {
-        //     errors.products = true;
-        //     productsError.style.display = 'block';
-        // }
-
-        // if (!formData.size) {
-        //     errors.size = true;
-        //     sizeError.style.display = 'block';
-        // }
-
-        if(optionProduct == true){
+        if(formData.products == "1"){
             errors.products = true;
             productsError.style.display = 'block';
         }
 
-        if(optionSize.value == true){
+
+        if(formData.size == "1"){
             errors.size = true;
             sizeError.style.display = 'block';
         }
-    }
 
-    if (!Object.values(errors).includes(true)) {
-        console.log(formData)
     }
+    
+
 })
+
